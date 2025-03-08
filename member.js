ba852ac6ef58e74c56a351db6e24f14286433bd7
Bm Iqbal Alam <bmiqbalalam0786@gmail.com>
2025-03-08 20:39:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    // This is the member's skills
+    let skills = ['HTML', 'CSS', 'JavaScript', 'React'];
+
+    // We can access the member's skills here
+    console.log(skills);
+}
